b3c4b71d2d96226592a70977fec3678322d80956
TobeTenkabito <three_yuan@outlook.com>
2026-02-12 16:22:10 +0800

    Add rendering extension to setup.py

--- services/tile_service/setup.py
+++ services/tile_service/setup.py
@@ -8,14 +8,24 @@ extensions = [
         ["engine/translator.pyx"],
         include_dirs=[numpy.get_include()],
         extra_compile_args=["-O3"],
-    )
+    ),
+    Extension(
+        "engine.rendering",
+        ["engine/rendering.pyx"],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=["-O3"],
+    ),
 ]
 
 setup(
     name="TileServiceEngine",
     ext_modules=cythonize(
         extensions,
-        compiler_directives={'language_level': "3", 'boundscheck': False, 'wraparound': False},
+        compiler_directives={
+            'language_level': "3",
+            'boundscheck': False,
+            'wraparound': False
+        },
         annotate=True
     ),
 )
